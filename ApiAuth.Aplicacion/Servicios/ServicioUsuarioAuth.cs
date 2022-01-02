c91e105d4a93db22e41b28c9680eea4b2b901632
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-01-02 13:36:11 -0600

    Proyecto base

---
+++ ApiAuth.Aplicacion/Servicios/ServicioUsuarioAuth.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiAuth.Aplicacion
+{
+    public class ServicioUsuarioAuth : IServicioUsuarioAuth
+    {
+        public void ValidarUsuario(string usuario, string contrasena)
+        {
+
+            var edad = 3;
+        }
+    }
+}
