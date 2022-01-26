b59fff5fe84da799f7adae32362714c6d056a19d
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-01-26 17:20:58 -0600

    Se agregan referencias y repositorio para validación de usuario.

---
+++ ApiAuth.Dominio/AgregadosUsuario/Usuario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiAuth.Dominio
+{
+    public class Usuario
+    {
+        Guid Id { get; set; }
+        string TokenSesion { get; set; }
+        string Correo { get; set; }
+        string Contrasenia { get; set; }
+
+        public Usuario(Guid id, string tokenSesion, string correo, string contrasenia)
+        {
+            Id = id;
+            TokenSesion = tokenSesion;
+            Correo = correo;
+            Contrasenia = contrasenia;
+        }
+
+
+    }
+}
---
+++ ApiAuth.Infraestructura/Repositorios/ConsultarUsuarios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ApiAuth.Dominio;
+
+namespace ApiAuth.Infraestructura.Repositorios
+{
+    public class ConsultarUsuarios : IConsultarUsuarios
+    {
+        public void UsuarioPorId()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
