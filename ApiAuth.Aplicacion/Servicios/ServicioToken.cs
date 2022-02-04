c5fe3468f070f35f680917821f6fbf5e90284fe6
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-02-04 13:38:03 -0600

    Se agrega clase Token

--- ApiAuth.Aplicacion/Servicios/ServicioToken.cs
+++ ApiAuth.Aplicacion/Servicios/ServicioToken.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ApiAuth.Dominio;
 using System.Threading.Tasks;
 
 namespace ApiAuth.Aplicacion
 {
     public class ServicioToken : IServicioToken
     {
+
         public string GenerarToken()
         {
             var token = string.Empty;
@@ -18,6 +20,11 @@ namespace ApiAuth.Aplicacion
             token += fecha.Minute.ToString() + fecha.Second + fecha.Millisecond;
 
             return token;
+        }
+
+        public void GuardarToken(Token token)
+        {
+
         }
     }
 }
---
+++ ApiAuth.Dominio/AgregadosUsuario/Token.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiAuth.Dominio
+{
+    public class Token
+    {
+        public string Valor { get; set; }
+        public DateTime FechaExpiracion { get; set; }
+        public Guid IdUsuario { get; set; }
+
+    }
+}
