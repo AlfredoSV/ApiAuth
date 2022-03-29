2bf9459038f40689271e4fd5eb430f0820dba878
Alfredo Sánchez <alfredosanchezverduzco@outlook.com>
2022-03-29 13:10:24 -0600

    Se módifica Startup para agregar cors de dominio https://localhost:44357/

--- ApiAuth/Startup.cs
+++ ApiAuth/Startup.cs
@@ -54,7 +54,7 @@ namespace ApiAuth
                 options.AddPolicy(name: MyAllowSpecificOrigins,
                     builder =>
                     {
-                        builder.WithOrigins("");
+                        builder.WithOrigins("https://localhost:44357/");
                     });
 
             });
