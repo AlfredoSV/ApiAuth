5c4a69b9cdd15ec5e781a15b20768fc526312f94
Alfredo Sánchez <45853187+AlfredoSV@users.noreply.github.com>
2023-10-15 15:17:12 -0600

    Was added DataAnnotations on  DtoUser

--- ApiAuth.Aplicacion/Dtos/DtoUser.cs
+++ ApiAuth.Aplicacion/Dtos/DtoUser.cs
@@ -1,14 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+
 
 namespace ApiAuth.Aplicacion.Dtos
 {
     public class DtoUser
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }
