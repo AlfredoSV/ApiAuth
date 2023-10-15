3b53ab1c900d1dc9e93da3d22e2d65d92695255b
Alfredo Sánchez <45853187+AlfredoSV@users.noreply.github.com>
2023-10-15 15:15:19 -0600

    Was added DataAnnotations on dto models

--- ApiAuth.Aplicacion/Dtos/DtoUsuarioLogin.cs
+++ ApiAuth.Aplicacion/Dtos/DtoUsuarioLogin.cs
@@ -1,20 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace ApiAuth.Aplicacion
 {
     public class DtoUserLogin
     {
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 
     public class DtoUserToken
     {
+        [Required]
         public Guid Id { get; set; }
+        [Required]
         public string Token { get; set; }
 
     }
